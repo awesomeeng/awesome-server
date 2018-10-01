bae004b8377cb09d2de43848ef62bed2e926ce14
Glen R. Goodwin <arei@usa.com>
2018-10-01 18:10:31 -0400

    Adds Multiple Routes example.

---
+++ examples/MultipleRoutes/Server.js
@@ -0,0 +1,36 @@
+// (c) 2018, The Awesome Engineering Company, https://awesomeneg.com
+
+"use strict";
+
+const Log = require("@awesomeeng/awesome-log");
+Log.start();
+
+const AwesomeServer = require("@awesomeeng/awesome-server");
+
+let server = new AwesomeServer();
+server.addHTTPServer({
+	host: "localhost",
+	port: 7080
+});
+
+// route one
+server.route("*","*",(path,request,response)=>{
+	response.writeHead(200);
+});
+
+// route two
+server.route("*","*hello*",async (path,request,response)=>{
+	await response.write("Hello\n");
+});
+
+// route three
+server.route("*","*world*",async (path,request,response)=>{
+	await response.write("World\n");
+});
+
+// route four
+server.route("*","*",async (path,request,response)=>{
+	await response.end();
+});
+
+server.start();
